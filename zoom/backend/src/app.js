53828f456b1c1046a949c5b49a2af1c2c8e9653a
ali-sorathiya64 <alisorathiya970@gmail.com>
2024-11-19 10:12:38 -0800

    add new files

--- zoom/backend/src/app.js
+++ zoom/backend/src/app.js
@@ -12,9 +12,10 @@ import userRoutes from "./routes/users.routes.js";
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
+const Connction_string ='mongodb://127.0.0.1:27017/ali-bhai'
 
 
-app.set("port", (process.env.PORT || 5000))
+app.set( process.env.PORT || 5000)
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
@@ -23,11 +24,11 @@ app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
     app.set("mongo_user")
-    const connectionDb = await mongoose.connect('mongodb://127.0.0.1:27017/ali-bhai')
+    const connectionDb = await mongoose.connect(`${process.env.MONGO_URL||Connction_string}`)
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
     server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 4000")
+        console.log("LISTENIN ON PORT 5000")
     });
 
 
