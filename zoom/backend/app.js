8dc5fa8ea45fdbf682fd01f0b8fe83ef6043c6fa
ali-sorathiya64 <alisorathiya970@gmail.com>
2024-11-27 04:19:56 -0800

    new add

--- zoom/backend/app.js
+++ zoom/backend/app.js
@@ -12,12 +12,13 @@ import userRoutes from "./src/routes/users.routes.js";
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
+const PORT = process.env.PORT || 5000;
 const Connction_string ='mongodb+srv://aasorathiya:nhuDIF2YtIjDTJIQ@cluster0.obszg.mongodb.net/conference-App'
 if ( process.env.NODE_ENV === 'production' && ! process.stdin){process.stdin ={ on :()=> {}, resume:() => {} };
 }
    
 
-app.set( process.env.PORT || 5000)
+
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
@@ -29,7 +30,7 @@ const start = async () => {
     const connectionDb = await mongoose.connect(process.env.MONGO_URL||Connction_string)
 
     console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
+    app.listen(PORT,app.get("port"), () => {
         console.log("LISTENIN ON PORT 5000")
     });
 
